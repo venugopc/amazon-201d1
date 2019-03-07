952ff52931ebf2c3b35329d8208abd5b27542d89
venu <venugopc@amazon.com>
2019-03-07 18:20:25 +0530

    multiplyanyarray requirment

--- class-05-git-branching-images-text/lab/app.js
+++ class-05-git-branching-images-text/lab/app.js
@@ -134,10 +134,20 @@ Test this function by hand in the console to get it working, and when you think 
 var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+    var multiplicationResult = 1;
+    dynamicArray.forEach(function (currentValue) {
+    multiplicationResult = multiply (multiplicationResult,currentValue )[0];
+    }) ;
+
+    var allNumbers = dynamicArray.join();
+
+    var returnStatement = 'The numbers ' + allNumbers + ' have a product of ' + multiplicationResult + '.';
+
+    return [multiplicationResult, returnStatement];
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
